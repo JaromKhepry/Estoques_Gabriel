aa8f7d0417ed2e3650f4bba3b2208246f6ea34e6
KepryDeSousa <Jarom.kepri@frosty.com.br>
2025-03-13 17:33:19 -0300

    up

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ else:
     
     if not st.session_state.registros.empty:
         output = BytesIO()
-        st.session_state.registros.to_excel(output, index=False, sheet_name='Resultados Lojas')
+       # st.session_state.registros.to_excel(output, index=False, sheet_name='Resultados Lojas')
         output.seek(0)
         st.download_button(
             label="Baixar Registros em excel",
